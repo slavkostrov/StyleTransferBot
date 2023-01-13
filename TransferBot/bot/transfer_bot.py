7412d89519f1344348e78e0612c1b76f1d6276b9
Slava <slavkostrov@gmail.com>
2023-01-13 18:51:30 +0300

    [UPDATE] Some improvements in bot and model selecting.

--- TransferBot/bot/transfer_bot.py
+++ TransferBot/bot/transfer_bot.py
@@ -13,8 +13,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.types.input_file import InputFile
 from aiogram.utils.callback_data import CallbackData
 
-from TransferBot.model import VGGTransfer
-from TransferBot.model.protocol import ModelABC
+from TransferBot.model import MODEL_REGISTRY
 from bot_answers import welcome_message
 
 logging.basicConfig(level=logging.INFO)
@@ -31,7 +30,24 @@ def process_func(queue: multiprocessing.Queue, model_class: type, image: BytesIO
 
 _CACHE = {}
 
-RequestAction = CallbackData('r', 'model', 'photo_id')
+RequestAction = CallbackData('r', 'model', 'request_id')
+
+
+@dataclass
+class Request:
+    chat_id: int
+    message_id: int
+    content_file_id: tp.Union[int, tp.List[int]]
+    style_file_id: tp.Union[int, tp.List[int], None] = field(default=None)
+
+    def __post_init__(self):
+        _CACHE[str(hash(self))] = self
+
+    def __hash__(self):
+        return hash(f"{self.chat_id}_{self.message_id}_{self.content_file_id}_{self.style_file_id}")
+
+    # TODO: timeout and delete messages after it
+    # def __del__
 
 
 @dataclass
@@ -56,18 +72,14 @@ class RequestsQueue:
 class TransferBot:
     """Класс бота для переноса стиля."""
 
-    def __init__(self, bot_token: str, model: ModelABC, max_tasks: int = 2):
+    def __init__(self, bot_token: str, max_tasks: int = 2):
         """
         Конструктор бота для переноса стиля.
 
         :param bot_token: Telegram токен бота.
-        :param model: класс модели для переноса стиля. # TODO: deprecate and let the user select it
         :param max_tasks: максимальное количество асинхронных задач переноса.
         """
-        if not isinstance(model, ModelABC):
-            raise RuntimeError("Not valid model.")
 
-        self.model = model
         self.bot = Bot(token=bot_token)
         self.max_tasks = max_tasks
         self.dispatcher = Dispatcher(self.bot)
@@ -79,7 +91,7 @@ class TransferBot:
         """Выполняет установку всех обработчиков."""
         self.dispatcher.register_message_handler(self.send_welcome, commands=["start", "help"])
         self.dispatcher.register_message_handler(self.process_photo, content_types=["photo"])
-        self.dispatcher.register_callback_query_handler(self._photo_process, lambda c: True)
+        self.dispatcher.register_callback_query_handler(self.apply_style)
 
     def run(self):
         """Запускает бота."""
@@ -91,26 +103,20 @@ class TransferBot:
         LOGGER.info(f"Sending welcome message to {message.chat.id}.")
         await message.reply(welcome_message.format(message=message), parse_mode='MarkdownV2')
 
-    async def _photo_process(self, query: types.CallbackQuery):
+    async def apply_style(self, query: types.CallbackQuery):
+        _, model_id, request_id = query.data.split(":")
+        request = _CACHE.pop(request_id)
 
-        _, model, photo_id = query.data.split(":")
-        chat_id = query.from_user.id
-        message_id = query.message.message_id - 1
-
-        await self.bot.edit_message_reply_markup(
-            chat_id=query.from_user.id,
+        await self.bot.edit_message_text(
+            text=f"Выбрана модель {model_id}.",
+            reply_markup=None,
             message_id=query.message.message_id,
-            reply_markup=None
+            chat_id=request.chat_id,
         )
 
-        await self.bot.edit_message_text(text=f"Выбрана модель {model}", reply_markup=None, message_id=message_id + 1,
-                                         chat_id=chat_id)
-
-        # TODO: fix
-        photo_id = _CACHE.pop(photo_id)
-        model = globals()[model]  # TODO: FIX
-        reply_message = await self._wait_in_queue(chat_id, message_id)
-        input_image = await self.download_image(photo_id)
+        model = MODEL_REGISTRY.get(model_id)  # TODO: FIX, make model registry
+        reply_message = await self._wait_in_queue(request.chat_id, request.message_id)
+        input_image = await self.download_image(request.content_file_id)
 
         queue = multiprocessing.Queue()
         process = multiprocessing.Process(target=process_func, args=(queue, model, input_image), )
@@ -131,41 +137,29 @@ class TransferBot:
                 raise Exception("process is dead")
             await asyncio.sleep(1)
 
-        await self.bot.delete_message(chat_id, reply_message.message_id)
-
+        await self.bot.delete_message(request.chat_id, reply_message.message_id)
+        await self.bot.delete_message(request.chat_id, query.message.message_id)
         await self.bot.send_photo(
-            chat_id=chat_id,
+            chat_id=request.chat_id,
             photo=InputFile(transformed_image),
-            caption="Результат переноса стиля!",
-            reply_to_message_id=message_id,
+            caption=f"Результат переноса стиля c использованием {model_id}!",
+            reply_to_message_id=request.message_id,
         )
-        self.queue.remove(message_id)
+        self.queue.remove(request.message_id)
 
     async def process_photo(self, message: types.Message):
         """Реализует логику по обработке фотографий."""
-        keyboard = InlineKeyboardMarkup()
-        _CACHE[str(hash(message.photo[-1].file_id))] = message.photo[-1].file_id
-        button = InlineKeyboardButton(
-            'VGG16!',
-            callback_data=RequestAction.new(
-                model="VGGTransfer",
-                photo_id=hash(message.photo[-1].file_id),
-            )
+        request = Request(
+            chat_id=message.chat.id,
+            message_id=message.message_id,
+            content_file_id=message.photo[-1].file_id,
         )
-        keyboard.insert(button)
-        button = InlineKeyboardButton(
-            'VGG19!',
-            callback_data=RequestAction.new(
-                model="VGG19Transfer",
-                photo_id=hash(message.photo[-1].file_id),
-            )
-        )
-        keyboard.insert(button)
-
+        keyboard: InlineKeyboardMarkup = self.make_keyboard(hash(request))
         await self.bot.send_message(
             chat_id=message.chat.id,
             text=f"Выберите модель:",
             reply_markup=keyboard,
+            reply_to_message_id=message.message_id,
         )
 
     async def _wait_in_queue(self, chat_id: int, message: int):
@@ -199,12 +193,29 @@ class TransferBot:
         await file_obj.download(byte_stream)
         return byte_stream
 
+    @staticmethod
+    def make_keyboard(request_id: int):
+        keyboard = InlineKeyboardMarkup()
+        button = InlineKeyboardButton(
+            'VGG16!',
+            callback_data=RequestAction.new(
+                model="VGGTransfer",
+                request_id=request_id,
+            )
+        )
+        keyboard.insert(button)
+        button = InlineKeyboardButton(
+            'VGG19!',
+            callback_data=RequestAction.new(
+                model="VGG19Transfer",
+                request_id=request_id,
+            )
+        )
+        keyboard.insert(button)
+        return keyboard
+
 
 if __name__ == '__main__':
     from secret import TOKEN
-
-    bot = TransferBot(
-        TOKEN,
-        model=VGGTransfer,
-    )
+    bot = TransferBot(TOKEN)
     bot.run()
