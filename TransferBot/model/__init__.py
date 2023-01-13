0f594e027464caf29e092e16cb335a57e2ad8c61
Slava <slavkostrov@gmail.com>
2023-01-13 18:51:30 +0300

    [UPDATE] Some improvements in bot and model selecting.

--- TransferBot/model/__init__.py
+++ TransferBot/model/__init__.py
@@ -1,2 +1,7 @@
 from .protocol import ModelABC, MockModel
 from .vgg import VGGTransfer
+from .vgg2 import VGG19Transfer
+
+MODEL_REGISTRY = {
+    value.__name__: value for value in ModelABC.__subclasses__()
+}
